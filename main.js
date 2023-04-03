82b198b2e39a148963360301fbe06ba14a95b5ee
levilu800b <levilu800b@gmail.com>
2023-04-03 21:14:00 +0100

    Changes made

--- main.js
+++ main.js
@@ -4,127 +4,169 @@ import './style.css';
 
 let things = JSON.parse(localStorage.getItem('things')) || [];
 
+let editingThing = null;
+
+
 function addThing(event) {
-    event.preventDefault();
-    const userName = document.getElementById('user-name').value.trim();
-    const name = document.getElementById('name').value.trim();
-    const powerLevel = document.getElementById('power-level').value.trim();
-    const description = document.getElementById('description').value.trim();
-    const uniqueProperty = document.getElementById('unique-property').value.trim();
-    const imageInput = document.getElementById('image-input');
-    const category = document.getElementById('category').value.trim();
+	event.preventDefault();
+	const userName = document.getElementById('user-name').value.trim();
+	const name = document.getElementById('name').value.trim();
+	const powerLevel = document.getElementById('power-level').value.trim();
+	const description = document.getElementById('description').value.trim();
+	const uniqueProperty = document
+		.getElementById('unique-property')
+		.value.trim();
+	const imageInput = document.getElementById('image-input');
+	const category = document.getElementById('category').value.trim();
 
-    if (!name || !description || isNaN(powerLevel) || !uniqueProperty || !userName) {
-        console.log('Invalid input. Please enter all properties with appropriate validation.');
-        return;
-    }
+	if (
+		!name ||
+		!description ||
+		isNaN(powerLevel) ||
+		!uniqueProperty ||
+		!userName
+	) {
+		console.log(
+			'Invalid input. Please enter all properties with appropriate validation.',
+		);
+		return;
+	}
 
-    let thing = things.find(thing => thing.uniqueProperty === uniqueProperty);
-    if (thing) {
+	let thing;
+	if (editingThing) {
+		// Update existing thing
+		thing = editingThing;
+	} else {
+		// Create new thing
+		thing = {
+			id: Date.now().toString(),
+			userName,
+			name,
+			powerLevel,
+			description,
+			uniqueProperty,
+			image: '',
+			category,
+		};
+		things.push(thing);
+	}
 
-        thing.userName = userName;
-        thing.name = name;
-        thing.powerLevel = powerLevel;
-        thing.description = description;
-        thing.category = category;
-        if (imageInput.files[0]) {
-            const reader = new FileReader();
-            reader.addEventListener('load', () => {
-                thing.image = reader.result;
-                localStorage.setItem('things', JSON.stringify(things));
-                renderThing(thing);
-            });
-            reader.readAsDataURL(imageInput.files[0]);
-        } else {
-            localStorage.setItem('things', JSON.stringify(things));
-            renderThing(thing);
-        }
-    } else {
+	thing.userName = userName;
+	thing.name = name;
+	thing.powerLevel = powerLevel;
+	thing.description = description;
+	thing.category = category;
+	if (imageInput.files[0]) {
+		const reader = new FileReader();
+		reader.addEventListener('load', () => {
+			thing.image = reader.result;
+			localStorage.setItem('things', JSON.stringify(things));
+			renderThing(thing);
+		});
+		reader.readAsDataURL(imageInput.files[0]);
+	} else {
+		localStorage.setItem('things', JSON.stringify(things));
+		renderThing(thing);
+	}
 
-        const reader = new FileReader();
-        reader.addEventListener('load', () => {
-            const image = reader.result;
-            thing = {
-                id: Date.now().toString(),
-                userName,
-                name,
-                powerLevel,
-                description,
-                uniqueProperty,
-                image,
-                category
-            };
-            things.push(thing);
-            localStorage.setItem('things', JSON.stringify(things));
-            renderThing(thing);
-        });
-        reader.readAsDataURL(imageInput.files[0]);
-    }
-
-    document.getElementById('add-form').reset();
+	editingThing = null;
+	document.getElementById('add-form').reset();
 }
+
 
 function renderThing(thing) {
-    const row = document.getElementById(thing.id);
-    if (row) {
-        row.cells[0].textContent = thing.userName;
-        row.cells[1].textContent = thing.name;
-        row.cells[2].textContent = thing.powerLevel;
-        row.cells[3].textContent = thing.description;
-        row.cells[5].firstChild.src = thing.image;
-        row.cells[6].textContent = thing.category;
-    } else {
-        const thingList = document.getElementById('thing-list');
-       const row = thingList.insertRow();
-        row.id = thing.id;
-        row.classList.add(things.length % 2 === 0 ? 'even' : 'odd');
+	const thingList = document.getElementById('thing-list');
+	const card = document.createElement('div');
+	card.classList.add('card');
 
-        const userNameCol = row.insertCell();
-        userNameCol.textContent = thing.userName;
-        const nameCol = row.insertCell();
-        nameCol.textContent = thing.name;
-        const powerLevelCol = row.insertCell();
-        powerLevelCol.textContent = thing.powerLevel;
-        const descriptionCol = row.insertCell();
-        descriptionCol.textContent = thing.description;
-        const uniquePropertyCol = row.insertCell();
-        uniquePropertyCol.textContent = thing.uniqueProperty;
-        const imageCol = row.insertCell();
-        const image = new Image();
-        image.src = thing.image;
-        image.width = 200;
-        imageCol.appendChild(image);
-        const categoryCol = row.insertCell();
-        categoryCol.textContent = thing.category;
-        const editCol = row.insertCell();
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener('click', () => editThing(thing));
-        editCol.appendChild(editButton);
+	const image = new Image();
+	image.classList.add('card-img-top');
+	image.src = thing.image;
 
-        const deleteCol = row.insertCell();
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => deleteThing(thing));
-        deleteCol.appendChild(deleteButton);
-    }}
+	const cardBody = document.createElement('div');
+	cardBody.classList.add('card-body');
+
+	const userName = document.createElement('h3');
+	userName.classList.add('card-text');
+	userName.textContent = `User Name: ${thing.userName}`;
+
+	const title = document.createElement('h5');
+	title.classList.add('card-title');
+	title.textContent = thing.name;
+
+	const subtitle = document.createElement('h6');
+	subtitle.classList.add('card-subtitle', 'mb-2', 'text-muted');
+	subtitle.textContent = `Power Level: ${thing.powerLevel}`;
+
+	const description = document.createElement('p');
+	description.classList.add('card-text');
+	description.textContent = thing.description;
+
+	const uniqueProperty = document.createElement('p');
+	uniqueProperty.classList.add('card-text');
+	uniqueProperty.textContent = `Unique Property: ${thing.uniqueProperty}`;
+
+	const category = document.createElement('p');
+	category.classList.add('card-text');
+	category.textContent = `Category: ${thing.category}`;
+
+	const editButton = document.createElement('button');
+	editButton.classList.add('btn', 'btn-primary', 'me-2');
+	editButton.textContent = 'Edit';
+	editButton.addEventListener('click', () => editThing(thing));
+
+	const deleteButton = document.createElement('button');
+	deleteButton.classList.add('btn', 'btn-danger');
+	deleteButton.textContent = 'Delete';
+	deleteButton.addEventListener('click', () => deleteThing(thing));
+
+	cardBody.appendChild(userName);
+	cardBody.appendChild(title);
+	cardBody.appendChild(subtitle);
+	cardBody.appendChild(description);
+	cardBody.appendChild(uniqueProperty);
+	cardBody.appendChild(category);
+	cardBody.appendChild(editButton);
+	cardBody.appendChild(deleteButton);
+
+	card.appendChild(image);
+	card.appendChild(cardBody);
+
+	if (editingThing && editingThing.id === thing.id) {
+		// If editingThing is set and the current thing is the one being edited, replace the old card with the new one
+		const oldCard = document.getElementById(`card-${editingThing.id}`);
+		thingList.replaceChild(card, oldCard);
+	} else {
+		// Otherwise, add the new card to the end of the list
+		card.setAttribute('id', `card-${thing.id}`);
+		thingList.appendChild(card);
+	}
+}
+
 
 function editThing(thing) {
-    document.getElementById('user-name').value = thing.userName;
-    document.getElementById('name').value = thing.name;
-    document.getElementById('power-level').value = thing.powerLevel;
-    document.getElementById('description').value = thing.description;
-    document.getElementById('unique-property').value = thing.uniqueProperty;
-    document.getElementById('category').value = thing.category;
-    document.getElementById('image-input').value = '';
+	editingThing = thing;
+	document.getElementById('user-name').value = thing.userName;
+	document.getElementById('name').value = thing.name;
+	document.getElementById('power-level').value = thing.powerLevel;
+	document.getElementById('description').value = thing.description;
+	document.getElementById('unique-property').value = thing.uniqueProperty;
+	document.getElementById('category').value = thing.category;
+	document.getElementById('image-input').value = '';
 }
+
+
 
 function deleteThing(thing) {
     const index = things.indexOf(thing);
     things.splice(index, 1);
     localStorage.setItem('things', JSON.stringify(things));
-    const row = document.getElementById(thing.id);
-    row.parentNode.removeChild(row);
+
+    const thingList = document.getElementById('thing-list');
+    thingList.innerHTML = '';
+    for (let thing of things) {
+        renderThing(thing);
+    }
 }
 
 document.getElementById('add-form').addEventListener('submit', addThing);
